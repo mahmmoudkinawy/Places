0d22830b19bf1eaae2105fc86b2ae7f486809704
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-14 17:39:42 +0200

    Added comment just for explention

--- API/Repositories/ParkRepository.cs
+++ API/Repositories/ParkRepository.cs
@@ -1,4 +1,6 @@
 ﻿namespace API.Repositories;
+
+//I did this class just if I wanna add some thing especial to it like RemoveRange ... etc
 public class ParkRepository : GenericRepository<Park>
 {
     public ParkRepository(DataContext context) : base(context)
