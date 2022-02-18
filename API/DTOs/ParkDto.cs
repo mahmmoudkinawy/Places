7fb988bce41bd50a4d2714f659a5d050efd19879
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-18 18:36:26 +0200

    Added picture field to the park entity'

--- API/DTOs/ParkDto.cs
+++ API/DTOs/ParkDto.cs
@@ -5,6 +5,7 @@ public class ParkDto
     public string Name { get; set; }
     public string State { get; set; }
     public DateTime Created { get; set; }
+    public byte[] Picture { get; set; }
     public DateTime Established { get; set; }
 }
 
