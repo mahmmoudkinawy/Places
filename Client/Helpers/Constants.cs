df72e8b48db3a87b5bca4aebff333910cb4963d8
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-19 10:09:52 +0200

    Added client project and make constants class

---
+++ Client/Helpers/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Client.Helpers;
+public static class Constants
+{
+    public static string APIBaseUrl = "https://localhost:44343/api";
+    public static string ParksPath = $"{APIBaseUrl}/Parks";
+    public static string TrailsPath = $"{APIBaseUrl}/Trails";
+}
